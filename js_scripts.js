974800b7b789bda6c33f824250a8abd50d9387a5
Shawn O'Connor <bartandfosto@gmail.com>
2016-05-18 15:20:13 -0400

    adds colors to JS script

--- js_scripts.js
+++ js_scripts.js
@@ -1,7 +1,7 @@
 console.log("The script is runnig!");
 
 function textChanger(event) {
-  randomColor= ["Gray", "Blue", "Red", "Yellow", "Green"]
+  randomColor= ["Gray", "Blue", "Red", "Yellow", "Green", "Teal", "Goldenrod"]
   var rand = randomColor[Math.floor(Math.random() * randomColor.length)];
   event.target.style.color = rand;
 }
